d6a424e6d70bf9a640fb3c5dca16fe8bd056d462
trystanlea <trystan.lea@gmail.com>
2013-10-07 09:33:09 +0100

    menu in dropdown

--- openbem_menu.php
+++ openbem_menu.php
@@ -4,6 +4,6 @@
   bindtextdomain($domain, "Modules/feed/locale");
   bind_textdomain_codeset($domain, 'UTF-8');
 
-  $menu_left[] = array('name'=> dgettext($domain, "OpenBEM"), 'path'=>"openbem/view" , 'session'=>"write", 'order' => 0 );
+  $menu_dropdown[] = array('name'=> dgettext($domain, "OpenBEM"), 'path'=>"openbem/monthly" , 'session'=>"write", 'order' => 0 );
 
 ?>
